b99d88df4f1ff0bda228abcf5ce9b34de23ec15a
Sree Dev <69527702+SreeDev-4522@users.noreply.github.com>
2021-06-24 13:53:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var i = p * y * r / 100;
+    var year = new Date().getFullYear() + parseInt(y);
+    var amount = i + p;
+    document.getElementById("result").innerHTML="If you deposit <mark>"+p+"\</mark>\, <br\>at an interest rate of <mark>"+r+"%\</mark>\, <br\>You will receive an amount of <mark>"+amount+"\</mark>\, <br\>in the year <mark>"+year+"\</mark> <br\>"
 }
-        
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}    
